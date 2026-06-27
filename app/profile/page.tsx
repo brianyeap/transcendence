98ed8c525565f28073f5e85c0af9f8517fdc6bff
Raja Vikraman <rajavikraman2006@gmail.com>
2026-06-27 21:52:32 +0800

    feat: Added some styling to the Username/header of the Profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -8,13 +8,20 @@ export default function ProfilePage() {
 		winPercentage: 58.3,
 	};
 	return (
-		<div>
-			<p> {userStats.username} </p>
-			<p> Games Played: {userStats.gamesPlayed}</p>
-			<p> Wins: {userStats.wins}</p>
-			<p> Losses: {userStats.losses}</p>
-			<p> Draws: {userStats.draws}</p>
-			<p> Win %: {userStats.winPercentage}</p>
+		<div className="p-8 text=[#eef2f8">
+			<h1 className="text-2x1 font-bold mb-6">{userStats.username}</h1>
+
+			<div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+				<div className="rounded-[7px] border border-white/[.07] bg-[#of131b] p-4">
+				<div className="text-xs uppercase tracking-wide text-[#5d6877]">Games Played</div>
+				<div className="text-xl font-semibold mt-1">{userStats.username}</div>
+			</div>
+				
+			 {/* {userStats.wins}</p>
+			{userStats.losses}</p>
+			{userStats.draws}</p>
+			 {userStats.winPercentage}</p> */}
+			 </div>
 		</div>
 	);
 }
