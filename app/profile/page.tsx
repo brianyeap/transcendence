283ff23fd4ed351a822632c021658fbc4f458fbc
Raja Vikraman <rajavikraman2006@gmail.com>
2026-06-27 21:19:19 +0800

    feat : display hardcoded user stats on profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,9 +1,15 @@
 export default function ProfilePage() {
+	const userStats = {
+		username: "Hello World",
+		gamesPlayed: 12,
+		wins: 7, 
+		losses: 4,
+		draws: 1,
+		winPercentage: 58.3,
+	};
 	return (
-		<>
-			<h1>PROFILE</h1>
-
-			<p>Just an empty page for now.</p>
-		</>
+		<div>
+			<p> {userStats.username} </p>
+		</div>
 	);
 }
